d0fcd98e0d9393c7f058da122c28e081d88d2d6f
James Dunkerley <james.dunkerley@gmail.com>
2021-09-22 22:19:27 +0100

    working LinReg

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use num::Num;
 
-fn compute_regression<I>(x_vals: I, y_vals: I) -> Option<(I::Item, I::Item)>
+fn compute_regression<I, T>(x_vals: I, y_vals: I) -> Option<(T, T)>
 where
-    I: IntoIterator,
-    I::Item: Num
+    I: IntoIterator<Item=T>,
+    T: Num + Copy
 {
     let mut x_iter = x_vals.into_iter();
     let mut y_iter = y_vals.into_iter();
@@ -11,9 +11,9 @@ where
     let mut x_opt = x_iter.next();
     let mut y_opt = y_iter.next();
 
-    let z = I::Item::zero();
-    let z1 = I::Item::one();
-    let accumulator : (I::Item, I::Item, I::Item, I::Item, I::Item) = (z, z, z, z, z);
+    let z = T::zero();
+    let z1 = T::one();
+    let mut accumulator : (T, T, T, T, T) = (z, z, z, z, z);
 
     while x_opt.is_some() && y_opt.is_some() {
         let x_val = x_opt.unwrap();
@@ -42,7 +42,7 @@ fn main() {
     let a = [1, 2, 3, 4];
     let b = [10, 8, 6, 4];
 
-    let reg = compute_regression(a, b);
+    let reg = compute_regression(a, b).unwrap();
     println!("{:?}", reg);
 
     println!("Hello, world!");
