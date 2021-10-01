ab2bd5e49f5f0a95f70d388604717ac7012f7571
James Dunkerley <james.dunkerley@gmail.com>
2021-10-01 10:34:44 +0100

    fix trait issue

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,17 +1,21 @@
-use std::io;
 use rand;
+use std::cmp::Ordering;
+use std::io;
 
-fn guess_game<T>(goes: u8) -> bool 
-where 
-    T: std::cmp::PartialOrd + std::fmt::Display + std::str::FromStr,
-    rand::distributions::Standard: rand::distributions::Distribution<T>
+fn guess_game<T>(goes: u8) -> bool
+where
+    T: std::fmt::Display + std::str::FromStr + Ord,
+    rand::distributions::Standard: rand::distributions::Distribution<T>,
 {
     let target: T = rand::random();
     let mut tries: u8 = 0;
 
     while tries < goes {
-        println!("Please input your guess (you have {} goes left).", goes - tries);
-        
+        println!(
+            "Please input your guess (you have {} goes left).",
+            goes - tries
+        );
+
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -19,28 +23,28 @@ where
 
         match guess.trim().parse::<T>() {
             Ok(num) => {
-                if num == target {
-                    println!("You win!");
-                    return true;
-                } else if num < target {
-                    println!("Too Low!");
-                } else {
-                    println!("Too High!");
+                match num.cmp(&target) {
+                    Ordering::Less => println!("Too small!"),
+                    Ordering::Greater => println!("Too big!"),
+                    Ordering::Equal => {
+                        println!("You win!");
+                        return true;
+                    }
                 }
+
                 tries += 1;
-            },
+            }
             Err(_) => {
                 println!("Your input '{}' is not an integer", guess);
             }
         }
     }
 
-    println!("You lose! The target was {}." , target);
+    println!("You lose! The target was {}.", target);
     return false;
 }
 
 fn main() {
     println!("Welcome to my guessing game...");
-    println!("You have 5 tries to guess the number I'm thinking of.");
-    guess_game::<u8>(5);
+    guess_game::<u8>(7);
 }
